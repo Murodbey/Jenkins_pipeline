936a662f610b357ef44a3f3803608fd64f9da1dc
Murodbey <abdullayevmurod@gmail.com>
2019-03-16 11:02:06 -0500

    git repo added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,21 @@
 node {
-    stage("Pull git")
-       git "git@github.com:Murodbey/website.git"
+    stage("Pull git"){
+        git "git@github.com:Murodbey/website.git"
+    }
+
+    stage("Install apache"){
+        sh "ssh ec2-user@${DEVIP}  sudo yum install httpd -y"
+    }
+
+    stage("start Apache"){
+        sh "ssh ec2-user@${DEVIP} 	 	sudo systemctl start httpd"
+    }
+
+    stage("Copy files"){
+        sh "rsync -aP --delete index.html ec2-user@${DEVIP}:/tmp"
+    }
+
+    stage("Move files"){
+        sh "ssh ec2-user@${DEVIP}       sudo cp -f /tmp/index.html /var/www/html/index.html"
+    }
 }
